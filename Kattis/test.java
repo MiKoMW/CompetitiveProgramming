efa181aa0579ecd87aeee8e4fc98d60839eb945b
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-10-13 22:48:49 +0100

    Updated
    Updated

---
+++ Kattis/test.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+/**
+ * Created by Mac on 2018/3/12.
+ */
+public class test {
+    public static void main(String[] args){
+        HashMap <Integer,String> ans = new HashMap<>();
+
+
+
+
+
+    }
+}
